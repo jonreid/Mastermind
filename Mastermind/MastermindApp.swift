b6a591662d912aed2d85d4d7df48b6e2cde145bd
Jon Reid <jon@qualitycoding.org>
2024-02-06 14:22:59 -0800

    initial commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Jon Reid on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
