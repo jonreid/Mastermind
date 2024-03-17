7c788087b9bdee5c97605fe3cc4ea8198d5200fb
Jon Reid <jon@qualitycoding.org>
2024-03-17 10:20:09 -0700

    F** spike red circle

--- Mastermind/ContentView.swift
+++ Mastermind/ContentView.swift
@@ -3,10 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle().fill(Color.red)
         }
         .padding()
     }
--- MastermindTests/PlaceholderTests.swift
+++ MastermindTests/PlaceholderTests.swift
@@ -1,8 +1,15 @@
 @testable import Mastermind
 import XCTest
+import SwiftUI
 
 @MainActor
 final class PlaceholderTests: XCTestCase {
     func test_zero() throws {
+    }
+}
+
+struct TestView: View {
+    var body: some View {
+        Circle()
     }
 }
