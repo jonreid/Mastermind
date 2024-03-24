ba1b5f23d4300edc18011331197b64a59d486409
Jon Reid <jon@qualitycoding.org>
2024-03-24 09:44:53 -0700

    R - Remove comments

--- MastermindTests/PlaceholderTests.swift
+++ MastermindTests/PlaceholderTests.swift
@@ -6,11 +6,8 @@ import SwiftUI
 @MainActor
 final class PlaceholderTests: XCTestCase {
     func test_circleIsRed() throws {
-        // make ContentView
         let view = ContentView()
-        // find Circle
         let color = try view.inspect().shape().foregroundColor()
-        // test that fill color is red
         XCTAssertEqual(color, Color.red)
     }
 }
