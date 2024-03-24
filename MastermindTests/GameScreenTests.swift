c2db035806505ccabe011993a1347dee9325f991
Jon Reid <jon@qualitycoding.org>
2024-03-24 09:50:25 -0700

    r - rename test suite

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -4,10 +4,12 @@ import ViewInspector
 import SwiftUI
 
 @MainActor
-final class PlaceholderTests: XCTestCase {
+final class GameScreenTests: XCTestCase {
     func test_circleIsRed() throws {
         let view = GameScreen()
+
         let color = try view.inspect().shape().foregroundColor()
+
         XCTAssertEqual(color, Color.red)
     }
 }
