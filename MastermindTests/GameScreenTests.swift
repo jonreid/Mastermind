7c79639c179013a27f651dbf01fa8b94f4dd40a5
Jon Reid <jon@qualitycoding.org>
2024-03-24 10:41:29 -0700

    t - move assertion out of closure

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -10,13 +10,13 @@ final class GameScreenTests: XCTestCase {
         var color = try sut.inspect().button().labelView().shape().foregroundColor()
         XCTAssertNotEqual(color, Color.orange, "Precondition")
 
-        let exp = sut.on(\.didAppear) { view in
+        sut.on(\.didAppear) { view in
             try view.button().tap()
 
             color = try view.button().labelView().shape().foregroundColor()
-            XCTAssertEqual(color, Color.orange)
         }
+
         ViewHosting.host(view: sut)
-        wait(for: [exp], timeout: 0.1)
+        XCTAssertEqual(color, Color.orange)
     }
 }
