b08920163768e252b81b66657209121f0374090d
Jon Reid <jon@qualitycoding.org>
2024-04-14 09:46:22 -0700

    t - wait for expectation

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -10,13 +10,14 @@ final class GameScreenTests: XCTestCase {
         var color = try sut.inspect().button().labelView().shape().foregroundColor()
         XCTAssertNotEqual(color, Color.orange, "Precondition")
 
-        sut.on(\.didAppear) { view in
+        let expectation = sut.on(\.didAppear) { view in
             try view.button().tap()
 
             color = try view.button().labelView().shape().foregroundColor()
         }
-
         ViewHosting.host(view: sut)
+        wait(for: [expectation], timeout: 0.01)
+
         XCTAssertEqual(color, Color.orange)
     }
 }
