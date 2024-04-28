b236d5583f8ecfc4d0a4bb979a8af1accb3c6e6d
Jon Reid <jon@qualitycoding.org>
2024-04-28 10:27:42 -0700

    t - pass file & line to display helper

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -5,8 +5,13 @@ import XCTest
 
 @MainActor
 final class GameScreenTests: XCTestCase {
-    private func display(_ sut: inout GameScreen, using: @escaping ((InspectableView<ViewType.View<GameScreen>>) throws -> Void)) {
-        let expectation = sut.on(\.viewInspectorHook, perform: using)
+    private func display(
+        _ sut: inout GameScreen,
+        using: @escaping ((InspectableView<ViewType.View<GameScreen>>) throws -> Void),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let expectation = sut.on(\.viewInspectorHook, file: file, line: line, perform: using)
         ViewHosting.host(view: sut)
         wait(for: [expectation], timeout: 0.01)
     }
