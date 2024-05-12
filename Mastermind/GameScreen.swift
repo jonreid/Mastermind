36991eef498175bd790148cd51f11494579a9424
Jon Reid <jon@qualitycoding.org>
2024-05-12 09:58:00 -0700

    - r Start using Key

--- Mastermind/GameScreen.swift
+++ Mastermind/GameScreen.swift
@@ -1,7 +1,14 @@
 import SwiftUI
 
+struct Key {
+    var color: Color
+}
+
+let key1 = Key(color: .blue)
+
 struct GameScreen: View {
     @State private var buttonColor = Color.red
+    @State private var guess1: Key?
     internal var viewInspectorHook: ((Self) -> Void)?
 
     var body: some View {
@@ -13,8 +20,9 @@ struct GameScreen: View {
             .id("guess1")
             Button(action: {
                 buttonColor = .blue
+                guess1 = key1
             }, label: {
-                Circle().foregroundColor(.blue)
+                Circle().foregroundColor(guess1?.color ?? .red)
             })
             .id("color1")
         }
--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -7,14 +7,14 @@ final class GameScreenTests: XCTestCase {
     func test_tappingBlueColorTurnsGuessBlue() throws {
         var sut = GameScreen()
         var color = try getColorOfGuess(try sut.inspect())
-        XCTAssertNotEqual(color, Color.blue, "Precondition")
+        XCTAssertNotEqual(color, key1.color, "Precondition")
 
         display(&sut) { view in
             try view.find(viewWithId: "color1").button().tap()
             color = try self.getColorOfGuess(view)
         }
 
-        XCTAssertEqual(color, Color.blue)
+        XCTAssertEqual(color, key1.color)
     }
 
     private func display(
