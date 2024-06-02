7977e085da90a399f0f7ad3e1deeea1e14a06e99
Jon Reid <jon@qualitycoding.org>
2024-06-02 09:58:35 -0700

    . f Start to style circles differently

--- Mastermind/GameScreen.swift
+++ Mastermind/GameScreen.swift
@@ -21,6 +21,8 @@ struct GameScreen: View {
             HStack {
                 Button(action: {}, label: {
                     Circle().foregroundColor(guess1?.color ?? .red)
+                        .frame(width: 100, height: 100)
+
                 })
                 .id("guess1")
                 CodeChoiceView(codePeg: codePeg1, id: "color1", guess: $guess1)
@@ -40,7 +42,13 @@ struct CodeChoiceView: View {
         Button(action: {
             guess = codePeg
         }, label: {
-            Circle().foregroundColor(codePeg.color)
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .frame(width: 100, height: 100)
+                .overlay(
+                    Circle().foregroundColor(codePeg.color)
+                        .padding(10)
+                )
         })
         .id(id)
     }
