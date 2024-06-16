4c09db7b50f017cc82170d0de06c23b64748f6f5
Jon Reid <jon@qualitycoding.org>
2024-06-16 10:44:49 -0700

    - f Pull back to 1 color for now

--- Mastermind/GameScreen.swift
+++ Mastermind/GameScreen.swift
@@ -6,8 +6,8 @@ struct CodeChoice {
 }
 
 let codeChoice1 = CodeChoice(color: .brown, codeValue: 1)
-let codeChoice2 = CodeChoice(color: .black, codeValue: 2)
-let codeChoices = [codeChoice2, codeChoice1]
+//let codeChoice2 = CodeChoice(color: .black, codeValue: 2)
+let codeChoices = [codeChoice1]
 
 struct GameScreen: View {
     @State private var guess1: CodeChoice?
