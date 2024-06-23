c4eeb6f5a0b95a09556dd2c6b31625b7461afc4d
Jon Reid <jon@qualitycoding.org>
2024-06-23 10:36:12 -0700

    - r Use color passed in

--- Mastermind/CodeChoice.swift
+++ Mastermind/CodeChoice.swift
@@ -1,8 +1,20 @@
 import SwiftUI
 
-struct CodeChoice {
+struct CodeChoice: Equatable {
     let color: Color
     let codeValue: Int
+}
+
+struct CodeChoiceGenerator {
+    static func generate(from colors: [Color], take count: Int) throws -> [CodeChoice] {
+//        guard colors.count >= count else {
+//            throw CodeChoiceGeneratorError.notEnoughColors
+//        }
+//        return colors.prefix(count).enumerated().map { index, color in
+        return colors.enumerated().map { index, color in
+            CodeChoice(color: color, codeValue: index + 1)
+        }
+    }
 }
 
 let codeChoice1 = CodeChoice(color: .brown, codeValue: 1)
--- MastermindTests/CodeChoiceTests.swift
+++ MastermindTests/CodeChoiceTests.swift
@@ -2,4 +2,8 @@
 import XCTest
 
 final class CodeChoiceTests: XCTestCase {
+    func test_generateOneCodeChoiceFromOneColor() throws {
+        let result = try CodeChoiceGenerator.generate(from: [.red], take: 1)
+        XCTAssertEqual(result, [CodeChoice(color: .red, codeValue: 1)])
+    }
 }
