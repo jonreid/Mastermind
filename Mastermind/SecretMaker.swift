4aa9b97d4c4146a985f860ee32ca2037351e37c1
Jon Reid <jon@qualitycoding.org>
2024-07-28 10:21:41 -0700

    - f normal SecretMaker also returns, not shuffled yet

--- Mastermind/SecretMaker.swift
+++ Mastermind/SecretMaker.swift
@@ -8,6 +8,6 @@ struct SecretMaker {
     private var isNull = false
 
     func makeSecret(from codeChoices: [CodeChoice]) -> [CodeChoice] {
-        return isNull ? codeChoices : []
+        return isNull ? codeChoices : codeChoices
     }
 }
--- MastermindTests/SecretMakerTests.swift
+++ MastermindTests/SecretMakerTests.swift
@@ -10,4 +10,17 @@ final class SecretMakerTests: XCTestCase {
 
         XCTAssertEqual(result, codeChoices)
     }
+
+    func test_notNull() throws {
+        let sut = SecretMaker()
+        let codeChoices = [CodeChoice(color: .black, codeValue: 1), CodeChoice(color: .blue, codeValue: 2)]
+
+        let result = sut.makeSecret(from: codeChoices)
+
+        XCTAssertEqual(result.count, codeChoices.count)
+        XCTAssertTrue(result.contains(codeChoices[0]))
+        XCTAssertTrue(result.contains(codeChoices[1]))
+    }
+
+    // Test that non-null SecretMaker returns array that is sometimes not equal to codeChoices
 }
