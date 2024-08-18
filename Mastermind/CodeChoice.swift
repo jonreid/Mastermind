8a7be53d999d1bcd44efe8f12906ee0c5d7aefdf
Jon Reid <jon@qualitycoding.org>
2024-08-18 10:13:43 -0700

    - r Extract method

--- Mastermind/CodeChoice.swift
+++ Mastermind/CodeChoice.swift
@@ -15,13 +15,19 @@ enum CodeChoiceGeneratorError: Error {
 
 struct CodeChoiceGenerator {
     static func generate(from colors: [Color], take count: Int) throws -> [CodeChoice] {
+        return try generate2(from: colors, take: count).options
+    }
+
+    static func generate2(from colors: [Color], take count: Int) throws -> CodeChoices {
         guard colors.count >= count else {
             throw CodeChoiceGeneratorError.notEnoughColors
         }
-        return colors.prefix(count).enumerated().map { index, color in
+        let result = colors.prefix(count).enumerated().map { index, color in
             CodeChoice(color: color, codeValue: index + 1)
         }
+        return CodeChoices(options: result)
     }
+
 }
 
 let codeColors: [Color] = [.brown, .black, .blue, .green, .yellow, .orange, .red, .gray]
