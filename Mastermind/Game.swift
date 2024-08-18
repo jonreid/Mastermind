22ae477cacfd7df74103bfce67da7ccb53f20dfd
Jon Reid <jon@qualitycoding.org>
2024-08-18 10:20:08 -0700

    - r set value directly without setter

--- Mastermind/Game.swift
+++ Mastermind/Game.swift
@@ -1,18 +1,15 @@
 final class Game {
-    private var _codeChoices: CodeChoices = CodeChoices(options: [])
+    private var _codeChoices: CodeChoices
     var codeChoices: [CodeChoice] {
         get {
             return _codeChoices.options
-        }
-        set {
-            _codeChoices = CodeChoices(options: newValue)
         }
     }
 
     var secret: [CodeChoice] = []
 
     init(numberOfCodeChoices: Int, _ secretMaker: SecretMaker) throws {
-        try codeChoices = CodeChoiceGenerator.generate(from: codeColors, take: numberOfCodeChoices).options
+        try _codeChoices = CodeChoiceGenerator.generate(from: codeColors, take: numberOfCodeChoices)
     }
 
     func makeNewSecret() {
