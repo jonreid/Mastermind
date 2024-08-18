7759c4e9b91bcdc495628b21e01bb3679dc22aa0
Jon Reid <jon@qualitycoding.org>
2024-08-18 10:51:03 -0700

    . r Rename property

--- Mastermind/Game.swift
+++ Mastermind/Game.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
 final class Game {
-    let _codeChoices: CodeChoices
+    let codeChoices: CodeChoices
 
     var secret: [CodeChoice] = []
 
     init(numberOfCodeChoices: Int, _ secretMaker: SecretMaker) throws {
-        try _codeChoices = CodeChoiceGenerator.generate(from: codeColors, take: numberOfCodeChoices)
+        try codeChoices = CodeChoiceGenerator.generate(from: codeColors, take: numberOfCodeChoices)
     }
 
     func codeChoice(_ index: Int) -> CodeChoice {
-        return _codeChoices.options[index]
+        return codeChoices.options[index]
     }
 
     func codeChoiceColor(_ index: Int) -> Color {
