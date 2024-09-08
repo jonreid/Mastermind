f3422d9420ff83184ceef00476ed0f7646f40d99
Jon Reid <jon@qualitycoding.org>
2024-09-08 10:15:33 -0700

    - f Guarantee code size restrictions

--- Mastermind/SecretMaker.swift
+++ Mastermind/SecretMaker.swift
@@ -12,6 +12,9 @@ struct SecretMaker {
     private var isNull = false
 
     func makeSecret(from codeChoices: CodeChoices, secretSize: Int) -> Secret {
+        assert(0 < secretSize)
+        assert(secretSize <= codeChoices.options.count)
+
         let colors = isNull ? codeChoices.options : codeChoices.options.shuffled()
         return Secret(code: Array(colors.prefix(secretSize)))
     }
