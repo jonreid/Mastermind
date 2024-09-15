78290f2117a76fa62eca2a9615c1c135cfb9af75
Jon Reid <jon@qualitycoding.org>
2024-09-15 10:18:03 -0700

    - r Keep passing Guess down

--- Mastermind/GameScreen.swift
+++ Mastermind/GameScreen.swift
@@ -58,7 +58,7 @@ private struct CodeChoicesView: View {
     var body: some View {
         VStack {
             ForEach(game.codeChoices.lastToFirst, id: \.codeValue) { codeChoice in
-                CodeChoiceView(codePeg: codeChoice, id: codeChoice.codeValue, guess1ToSet: $guess1)
+                CodeChoiceView(codePeg: codeChoice, id: codeChoice.codeValue, guess1ToSet: $guess1, guessToSet: $guess)
             }
         }
         .id("codeChoices")
@@ -69,6 +69,7 @@ private struct CodeChoiceView: View {
     var codePeg: CodeChoice
     var id: Int
     @Binding var guess1ToSet: CodeChoice?
+    @Binding var guessToSet: Guess
 
     var body: some View {
         Button(action: {
