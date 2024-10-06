fef7657fa926e0267edee5a2ba4be5101b4611f4
Jon Reid <jon@qualitycoding.org>
2024-10-06 10:37:12 -0700

    . t Extract color fixture

--- MastermindTests/SecretTests.swift
+++ MastermindTests/SecretTests.swift
@@ -2,24 +2,39 @@
 import XCTest
 
 final class SecretTests: XCTestCase {
+    private var red: CodeChoice!
+    private var green: CodeChoice!
+    private var blue: CodeChoice!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        red = CodeChoice(color: .red, codeValue: 1)
+        green = CodeChoice(color: .green, codeValue: 2)
+        blue = CodeChoice(color: .blue, codeValue: 3)
+    }
+
+    override func tearDownWithError() throws {
+        red = nil
+        green = nil
+        blue = nil
+        try super.tearDownWithError()
+    }
+
     func test_secretMatchesGuess() throws {
-        let red = CodeChoice(color: .red, codeValue: 1)
-        let green = CodeChoice(color: .green, codeValue: 2)
         let secret = Secret(code: [red, green])
         let guess = Guess(secretSize: 2)
         guess[0] = red
         guess[1] = green
+
         XCTAssertTrue(secret.matches(guess))
     }
 
     func test_secretDoesNotMatchGuess() throws {
-        let red = CodeChoice(color: .red, codeValue: 1)
-        let green = CodeChoice(color: .green, codeValue: 2)
-        let blue = CodeChoice(color: .blue, codeValue: 3)
         let secret = Secret(code: [red, green])
         let guess = Guess(secretSize: 2)
         guess[0] = red
         guess[1] = blue
+        
         XCTAssertFalse(secret.matches(guess))
     }
 }
