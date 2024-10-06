4c2c87d0452edec04cef31af4e439b3585db9d1d
Jon Reid <jon@qualitycoding.org>
2024-10-06 10:40:58 -0700

    - r Move Secret to own file

--- Mastermind/SecretMaker.swift
+++ Mastermind/SecretMaker.swift
@@ -1,19 +1,3 @@
-struct Secret {
-    let code: [CodeChoice]
-
-    func matches(_ guess: Guess) -> Bool {
-        guard code.count == guess.size else {
-            return false
-        }
-        for (index, secret) in code.enumerated() {
-            if guess[index] != secret {
-                return false
-            }
-        }
-        return true
-    }
-}
-
 struct SecretMaker {
     static func createNull() -> SecretMaker {
         var secretMaker = SecretMaker()
