0561db3c3fc931494fb26c583739f23816858835
Jon Reid <jon@qualitycoding.org>
2024-10-06 10:29:14 -0700

    . t Extract explaining variable

--- MastermindTests/SecretTests.swift
+++ MastermindTests/SecretTests.swift
@@ -3,10 +3,12 @@ import XCTest
 
 final class SecretTests: XCTestCase {
     func test_secretMatchesGuess() throws {
-        let secret = Secret(code: [CodeChoice(color: .red, codeValue: 1), CodeChoice(color: .green, codeValue: 2)])
+        let red = CodeChoice(color: .red, codeValue: 1)
+        let green = CodeChoice(color: .green, codeValue: 2)
+        let secret = Secret(code: [red, green])
         let guess = Guess(secretSize: 2)
-        guess[0] = CodeChoice(color: .red, codeValue: 1)
-        guess[1] = CodeChoice(color: .green, codeValue: 2)
+        guess[0] = red
+        guess[1] = green
         XCTAssertTrue(secret.matches(guess))
     }
 }
