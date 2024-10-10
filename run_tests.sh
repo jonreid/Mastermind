4a8d7dba3fad91013d86777aaa974f73e0fa8200
Jon Reid <jon@qualitycoding.org>
2024-10-10 10:00:01 -0700

    . e -disableAutomaticPackageResolution when calling xcodebuild on build server

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 SCHEME='Mastermind'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
 
-xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO'
