abe439a515082278363270074ade40adc7360ba6
Jon Reid <jon@qualitycoding.org>
2024-10-26 16:02:03 -0700

    . e Bump simulator to iPhone 16

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCHEME='Mastermind'
-DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
 xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO'
