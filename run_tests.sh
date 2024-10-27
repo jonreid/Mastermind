388a859fae0b5732ff2d8d3a065dbfe7fe728e79
Jon Reid <jon@qualitycoding.org>
2024-10-26 17:26:13 -0700

    . e Prewarm simulator before xcodebuild

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,7 @@
 SCHEME='Mastermind'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
+# Prewarm to avoid "Timed out trying to boot simulator"
+open -b com.apple.iphonesimulator
+
 set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
