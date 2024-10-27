8d5d43676fb542da386a026c7a43333cf2a44367
Jon Reid <jon@qualitycoding.org>
2024-10-26 17:55:08 -0700

    . e Add echo logs to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,8 @@ SCHEME='Mastermind'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
 # Prewarm to avoid "Timed out trying to boot simulator"
+echo "Prewarm simulator"
 open -b com.apple.iphonesimulator
 
+echo "xcodebuild test"
 set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
