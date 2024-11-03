173ee8575b266a29f09c53b2358114274aff3f55
Jon Reid <jon@qualitycoding.org>
2024-11-03 10:33:40 -0800

    . t Shift SecretMakerTests away from setUp

--- MastermindTests/SecretMakerTests.swift
+++ MastermindTests/SecretMakerTests.swift
@@ -2,14 +2,8 @@
 import XCTest
 
 final class SecretMakerTests: XCTestCase {
-    private var codeChoices: CodeChoices!
-
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        codeChoices = try CodeChoiceGenerator.generate(from: codeColors, take: 8)
-    }
-
     func test_nullable() throws {
+        let codeChoices = try makeCodeChoices()
         let sut = SecretMaker.createNull()
 
         let result = sut.makeSecret(from: codeChoices, secretSize: 8)
@@ -18,6 +12,7 @@ final class SecretMakerTests: XCTestCase {
     }
 
     func test_useSecretSize() throws {
+        let codeChoices = try makeCodeChoices()
         let sut = SecretMaker.createNull()
 
         let result = sut.makeSecret(from: codeChoices, secretSize: 2)
@@ -26,6 +21,7 @@ final class SecretMakerTests: XCTestCase {
     }
 
     func test_containsAllCodeChoices() throws {
+        let codeChoices = try makeCodeChoices()
         let sut = SecretMaker()
 
         let result = sut.makeSecret(from: codeChoices, secretSize: 8)
@@ -38,6 +34,7 @@ final class SecretMakerTests: XCTestCase {
     }
 
     func test_shufflesCodeChoices() throws {
+        let codeChoices = try makeCodeChoices()
         let sut = SecretMaker()
 
         for _ in 0..<100 {
@@ -47,5 +44,9 @@ final class SecretMakerTests: XCTestCase {
             }
         }
         XCTFail("Failed to shuffle code choices after 100 attempts")
+    }
+
+    private func makeCodeChoices() throws -> CodeChoices {
+        return try CodeChoiceGenerator.generate(from: codeColors, take: 8)
     }
 }
