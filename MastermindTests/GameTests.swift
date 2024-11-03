6b1fb0f778525856c2bed1d1dcdaee0468133f8e
Jon Reid <jon@qualitycoding.org>
2024-11-03 10:26:48 -0800

    . t Extract makeGame helper

--- MastermindTests/GameTests.swift
+++ MastermindTests/GameTests.swift
@@ -3,22 +3,26 @@ import XCTest
 
 final class GameTests: XCTestCase {
     func test_isObservable_soThatViewsCanObserveChanges() throws {
-        let game = try Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker.createNull())
+        let game = try makeGame()
 
         XCTAssertTrue(game is (any Observable))
     }
 
     func test_gameHasNoSecretInitially() throws {
-        let game = try Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker.createNull())
+        let game = try makeGame()
 
         XCTAssertEqual(game.secret.code, [])
     }
 
     func test_makeNewSecret() throws {
-        let game = try Game(numberOfCodeChoices: 2, secretSize: 1, SecretMaker.createNull())
+        let game = try makeGame()
 
         game.makeNewSecret()
 
         XCTAssertEqual(game.secret.code.first, game.codeChoices.options.first)
+    }
+
+    private func makeGame() throws -> Game {
+        try Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker.createNull())
     }
 }
