697dd9abf882ca673c26ef71bd638d0ec186fce4
Jon Reid <jon@qualitycoding.org>
2025-01-05 10:10:59 -0800

    . t Snapshot check button enabled

---
+++ Mastermind/UI/Tests/CheckButtonSnapshotTests.swift
@@ -0,0 +1,21 @@
+@testable import Mastermind
+import SnapshotTesting
+import SwiftUI
+import Testing
+
+@MainActor
+final class CheckButtonSnapshotTests: @unchecked Sendable {
+    @Test
+    func enabledColor() async throws {
+        let view = CheckButton().disabled(false)
+        assertSnapshot(of: view.toVC(), as: .image)
+    }
+}
+
+extension SwiftUI.View {
+    func toVC() -> UIViewController {
+        let vc = UIHostingController(rootView: self)
+        vc.view.frame = UIScreen.main.bounds
+        return vc
+    }
+}
