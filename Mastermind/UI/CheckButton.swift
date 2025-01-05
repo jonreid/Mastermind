7cfc09d8df6d52123227f0cd9bf185dd61b72138
Jon Reid <jon@qualitycoding.org>
2025-01-05 09:50:00 -0800

    . r Move CheckButton

---
+++ Mastermind/UI/CheckButton.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct CheckButton: View {
+    @Environment(\.isEnabled)
+    private var isEnabled: Bool
+
+    var body: some View {
+        Button(action: {
+        }, label: {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(isEnabled ? .blue : .unselected)
+                .frame(width: 100, height: 200)
+                .overlay(
+                    Text("Check")
+                        .foregroundColor(.white)
+                        .font(.title)
+                )
+        })
+        .tag("checkButton")
+    }
+}
--- Mastermind/UI/GameScreen.swift
+++ Mastermind/UI/GameScreen.swift
@@ -94,25 +94,6 @@ private extension CodeChoices {
     }
 }
 
-private struct CheckButton: View {
-    @Environment(\.isEnabled)
-    private var isEnabled: Bool
-
-    var body: some View {
-        Button(action: {
-        }, label: {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(isEnabled ? .blue : .unselected)
-                .frame(width: 100, height: 200)
-                .overlay(
-                    Text("Check")
-                        .foregroundColor(.white)
-                        .font(.title)
-                )
-        })
-        .tag("checkButton")
-    }
-}
 
 #Preview {
     let game = try! Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker.createNull())
