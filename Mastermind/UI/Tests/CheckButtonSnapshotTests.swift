8fa3724fd5e47c29de9a8de52f27e3209ad1d088
Jon Reid <jon@qualitycoding.org>
2025-01-11 14:54:18 -0800

    . t Increase snapshot precision

--- Mastermind/UI/Tests/CheckButtonSnapshotTests.swift
+++ Mastermind/UI/Tests/CheckButtonSnapshotTests.swift
@@ -8,13 +8,13 @@ final class CheckButtonSnapshotTests: @unchecked Sendable {
     @Test
     func disabledColor() async throws {
         let view = CheckButton().disabled(true)
-        assertSnapshot(of: view.toVC(), as: .image(precision: 0.98, perceptualPrecision: 0.99))
+        assertSnapshot(of: view.toVC(), as: .image(precision: 0.99, perceptualPrecision: 0.99))
     }
 
     @Test
     func enabledColor() async throws {
         let view = CheckButton().disabled(false)
-        assertSnapshot(of: view.toVC(), as: .image(precision: 0.98, perceptualPrecision: 0.99))
+        assertSnapshot(of: view.toVC(), as: .image(precision: 0.99, perceptualPrecision: 0.99))
     }
 }
 
