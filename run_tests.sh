c814b1abf2a84d82b56aa270ffd3fd49e0873133
Jon Reid <jon@qualitycoding.org>
2025-01-11 11:46:00 -0800

    . e Try 18.0 on runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCHEME='Mastermind'
-DESTINATION='platform=iOS Simulator,OS=18.1,name=iPhone 16'
+DESTINATION='platform=iOS Simulator,OS=18.0,name=iPhone 16'
 
 set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
