3a578d3bd26626444967cab083d144ae9780bd53
Jon Reid <jon@qualitycoding.org>
2025-01-12 10:25:18 -0800

    . t Many of 1 parameterization

--- Mastermind/Domain/Tests/FeedbackPegsTests.swift
+++ Mastermind/Domain/Tests/FeedbackPegsTests.swift
@@ -1,16 +1,13 @@
 @testable import Mastermind
 import Testing
 
+private let red = CodeChoice(color: .red, codeValue: 1)
+private let green = CodeChoice(color: .green, codeValue: 2)
+private let blue = CodeChoice(color: .blue, codeValue: 3)
+
 final class FeedbackPegsTests: @unchecked Sendable {
-    private let red = CodeChoice(color: .red, codeValue: 1)
-    private let green = CodeChoice(color: .green, codeValue: 2)
-    private let blue = CodeChoice(color: .blue, codeValue: 3)
-
-    @Test
-    func numberOfPegsIsCodeLength() async throws {
-        let secretCode = [red, green, blue]
-        let expectedCount = 3
-
+    @Test(arguments: [([red, green, blue], 3)])
+    func numberOfPegsIsCodeLength(secretCode: [CodeChoice], expectedCount: Int) async throws {
         let secret = Secret(code: secretCode)
 
         let feedbackPegs = FeedbackPegs(secret: secret)
