6923a09fc42097fd316147b320c265ec35663079
Jon Reid <jon@qualitycoding.org>
2025-01-12 10:44:36 -0800

    - r Move Function of guess evaluation

--- Mastermind/Domain/FeedbackEvaluator.swift
+++ Mastermind/Domain/FeedbackEvaluator.swift
@@ -6,15 +6,6 @@ struct FeedbackEvaluator {
     }
 
     func evaluate(_ guess: Guess) -> Feedback {
-        var inCorrectPosition = 0
-        var inWrongPosition = 0
-        for (index, secret) in secret.code.enumerated() {
-            if guess[index] == secret {
-                inCorrectPosition += 1
-            } else if guess.contains(secret) {
-                inWrongPosition += 1
-            }
-        }
-        return Feedback(inCorrectPosition: inCorrectPosition, inWrongPosition: inWrongPosition)
+        secret.evaluate(guess)
     }
 }
--- Mastermind/Domain/Secret.swift
+++ Mastermind/Domain/Secret.swift
@@ -9,6 +9,20 @@ struct Secret: CustomStringConvertible {
         code.count
     }
 
+    func evaluate(_ guess: Guess) -> Feedback {
+        var inCorrectPosition = 0
+        var inWrongPosition = 0
+        for (index, secret) in code.enumerated() {
+            if guess[index] == secret {
+                inCorrectPosition += 1
+            } else if guess.contains(secret) {
+                inWrongPosition += 1
+            }
+        }
+        return Feedback(inCorrectPosition: inCorrectPosition, inWrongPosition: inWrongPosition)
+    }
+
+
     func matches(_ guess: Guess) -> Bool {
         guard code.count == guess.size else {
             return false
