93d519789058f0ce760ac6d6c3cb14a51e851fb6
Jon Reid <jon@qualitycoding.org>
2025-02-09 09:48:44 -0800

    . t Change preview to 4x4

--- Mastermind/UI/GameScreen.swift
+++ Mastermind/UI/GameScreen.swift
@@ -96,6 +96,6 @@ private extension CodeChoices {
 
 
 #Preview {
-    let game = try! Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker.createNull())
+    let game = try! Game(numberOfCodeChoices: 4, secretSize: 4, SecretMaker.createNull())
     return GameScreen(game: game)
 }
