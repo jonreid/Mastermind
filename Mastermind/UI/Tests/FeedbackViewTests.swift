fd20ee24e35f434075eb682c72ffd3e13c4d9366
Jon Reid <jon@qualitycoding.org>
2025-02-16 10:36:50 -0800

    . t Define peg1Color accessor

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -1,4 +1,5 @@
 @testable import Mastermind
+import SwiftUI
 import ViewInspector
 import XCTest
 
@@ -7,13 +8,19 @@ final class FeedbackViewTests: XCTestCase, Sendable {
     func test_zero() throws {
         let sut = FeedbackView(feedbackPegs: [.empty, .empty, .empty, .empty])
 
-        try sut.inspect().peg1()
+        let peg1 = try sut.inspect().peg1()
+
+//        XCTAssertEqual(peg1.fillColor, Color.unselected)
     }
 }
 
 private extension InspectableView {
     func peg1() throws -> InspectableView<ViewType.Shape> {
         try find(viewWithAccessibilityIdentifier: "feedback1").shape()
+    }
+
+    func peg1Color() throws -> Color? {
+        try find(viewWithAccessibilityIdentifier: "feedback1").shape().foregroundColor()
     }
 }
 
