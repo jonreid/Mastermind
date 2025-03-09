c8a8d6cfbafa218724efc54b7bf706cc226aa6eb
Jon Reid <jon@qualitycoding.org>
2025-03-09 10:18:25 -0700

    . t Remove now-unneeded test

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -5,20 +5,6 @@ import XCTest
 
 @MainActor
 final class FeedbackViewTests: XCTestCase, Sendable {
-    func test_pegs_allEmpty() throws {
-        let sut = FeedbackView(feedbackPegs: [.empty, .empty, .empty, .empty])
-
-        let peg1Color = try sut.inspect().pegColor(1)
-        let peg2Color = try sut.inspect().pegColor(2)
-        let peg3Color = try sut.inspect().pegColor(3)
-        let peg4Color = try sut.inspect().pegColor(4)
-
-        XCTAssertEqual(peg1Color, Color.Pegs.unselected)
-        XCTAssertEqual(peg2Color, Color.Pegs.unselected)
-        XCTAssertEqual(peg3Color, Color.Pegs.unselected)
-        XCTAssertEqual(peg4Color, Color.Pegs.unselected)
-    }
-
     func test_pegs() throws {
         let sut = FeedbackView(feedbackPegs: [.correct, .misplaced, .misplaced, .empty])
 
