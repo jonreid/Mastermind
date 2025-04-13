a82695441e420f9054874e6fea1fffc18915e7bf
Jon Reid <jon@qualitycoding.org>
2025-04-13 10:39:33 -0700

    . t Improve test names

--- Mastermind/UI/Tests/FeedbackPeg+ColorTests.swift
+++ Mastermind/UI/Tests/FeedbackPeg+ColorTests.swift
@@ -4,14 +4,14 @@ import Testing
 
 final class FeedbackPeg_ColorTests: @unchecked Sendable {
     @Test
-    func overlayColorDependingOnWhetherThePegIsFilledOrNot_empty() async throws {
+    func colorAroundPegToShowThatPegIsFilled_forEmptyPeg() async throws {
         let sut = FeedbackPeg.empty
         let color = sut.colorAroundPegToShowThatPegIsFilled()
         #expect(color == Color.clear)
     }
 
     @Test
-    func overlayColorDependingOnWhetherThePegIsFilledOrNot_correct() async throws {
+    func colorAroundPegToShowThatPegIsFilled_forFilledPeg() async throws {
         let sut = FeedbackPeg.correct
         let color = sut.colorAroundPegToShowThatPegIsFilled()
         #expect(color == Color.Pegs.correct)
