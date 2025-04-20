f7b55b8111c80d2af4076ea9e5fda92f1b492ed6
mjb <mohamadjavadbashtani@gmail.com>
2025-04-20 04:23:28 +0330

    -t Test color of around pegs fills correctly

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -6,7 +6,7 @@ import XCTest
 @MainActor
 final class FeedbackViewTests: XCTestCase, Sendable {
     func test_pegs() throws {
-        let sut = FeedbackView(feedbackPegs: [.correct, .misplaced, .misplaced, .empty])
+        let sut = makeSUT(feedbackPegs: [.correct, .misplaced, .misplaced, .empty])
 
         let peg1Color = try sut.inspect().pegColor(1)
         let peg2Color = try sut.inspect().pegColor(2)
@@ -18,11 +18,37 @@ final class FeedbackViewTests: XCTestCase, Sendable {
         XCTAssertEqual(peg3Color, Color.Pegs.misplaced)
         XCTAssertEqual(peg4Color, Color.Pegs.unselected)
     }
+    
+    func test_colorAroundOfPegsFillsCorrectlyForEmptyAndFilledPegs() throws {
+        let sut = makeSUT(feedbackPegs: [.correct, .misplaced, .misplaced, .empty])
+
+        let peg1Color = try sut.inspect().pegStrokeColor(1)
+        let peg2Color = try sut.inspect().pegStrokeColor(2)
+        let peg3Color = try sut.inspect().pegStrokeColor(3)
+        let peg4Color = try sut.inspect().pegStrokeColor(4)
+
+        XCTAssertEqual(peg1Color, Color.Pegs.correct)
+        XCTAssertEqual(peg2Color, Color.Pegs.misplaced)
+        XCTAssertEqual(peg3Color, Color.Pegs.misplaced)
+        XCTAssertEqual(peg4Color, Color.clear)
+    }
+    
+    private func makeSUT(feedbackPegs: [FeedbackPeg]) -> FeedbackView {
+        FeedbackView(feedbackPegs: feedbackPegs)
+    }
 }
 
 private extension InspectableView {
+    func pegShape(_ peg: Int) throws -> InspectableView<ViewType.Shape> {
+        try find(viewWithAccessibilityIdentifier: "feedback\(peg)").shape()
+    }
+    
     func pegColor(_ peg: Int) throws -> Color? {
-        try find(viewWithAccessibilityIdentifier: "feedback\(peg)").shape().foregroundColor()
+        try pegShape(peg).foregroundColor()
+    }
+    
+    func pegStrokeColor(_ peg: Int) throws -> Color? {
+        try pegShape(peg).overlay().shape().fillShapeStyle(Color.self)
     }
 }
 
