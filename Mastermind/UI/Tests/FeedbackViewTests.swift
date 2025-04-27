2c7866a7af5f3fa39907c99762ee5afe1a4e4ba5
Jon Reid <jon@qualitycoding.org>
2025-04-27 10:18:03 -0700

    . t test_colorAroundPegsShowingFilledVersusEmpty

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 @MainActor
 final class FeedbackViewTests: XCTestCase, Sendable {
-    func test_pegs() throws {
+    func test_pegColor() throws {
         let sut = makeSUT(.correct, .misplaced, .misplaced, .empty)
 
         let peg1Color = try sut.inspect().pegColor(1)
@@ -17,6 +17,20 @@ final class FeedbackViewTests: XCTestCase, Sendable {
         XCTAssertEqual(peg2Color, Color.Pegs.misplaced)
         XCTAssertEqual(peg3Color, Color.Pegs.misplaced)
         XCTAssertEqual(peg4Color, Color.Pegs.unselected)
+    }
+
+    func test_colorAroundPegsShowingFilledVersusEmpty() throws {
+        let sut = makeSUT(.correct, .misplaced, .misplaced, .empty)
+
+        let peg1Stroke = try sut.inspect().pegStrokeColor(1)
+        let peg2Stroke = try sut.inspect().pegStrokeColor(2)
+        let peg3Stroke = try sut.inspect().pegStrokeColor(3)
+        let peg4Stroke = try sut.inspect().pegStrokeColor(4)
+
+        XCTAssertEqual(peg1Stroke, Color.Pegs.correct)
+        XCTAssertEqual(peg2Stroke, Color.Pegs.misplaced)
+        XCTAssertEqual(peg3Stroke, Color.Pegs.misplaced)
+        XCTAssertEqual(peg4Stroke, Color.clear)
     }
 
     private func makeSUT(_ feedbackPegs: FeedbackPeg...) -> FeedbackView {
