da6f431b00c8335c87601430b7f4ed911fece72f
Jon Reid <jon@qualitycoding.org>
2025-05-18 10:31:11 -0700

    ^ r Try class instead of struct

--- Mastermind/Domain/Game.swift
+++ Mastermind/Domain/Game.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 
-struct Rounds {
+final class Rounds {
     var guesses: [Guess]
     var currentRound: Int = 0
+
+    init(guesses: [Guess]) {
+        self.guesses = guesses
+    }
 }
 
 @Observable
