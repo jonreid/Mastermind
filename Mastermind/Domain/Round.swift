bbdc055d517d158742f499b6bf06f3d99e688808
Jon Reid <jon@qualitycoding.org>
2025-07-20 09:42:26 -0700

    . r Rename to guess

--- Mastermind/Domain/Round.swift
+++ Mastermind/Domain/Round.swift
@@ -2,37 +2,37 @@ import Observation
 
 @Observable
 final class Round {
-    private var code: [CodeChoice?]
+    private var guess: [CodeChoice?]
     var feedbackPegs: [FeedbackPeg]
 
     init(secretSize: Int) {
-        code = Array(repeating: nil, count: secretSize)
+        guess = Array(repeating: nil, count: secretSize)
         feedbackPegs = Feedback.initial(size: secretSize).pegs
     }
 
     var size: Int {
-        code.count
+        guess.count
     }
 
     var isComplete: Bool {
-        !code.contains { $0 == nil }
+        !guess.contains { $0 == nil }
     }
 
     subscript(index: Int) -> CodeChoice? {
         get {
-            return code[index]
+            return guess[index]
         }
         set {
-            code[index] = newValue
+            guess[index] = newValue
         }
     }
 
     func placeChoiceInNextSlot(_ choice: CodeChoice) {
-        guard let index = code.firstIndex(of: nil) else { return }
-        code[index] = choice
+        guard let index = guess.firstIndex(of: nil) else { return }
+        guess[index] = choice
     }
 
     func contains(_ choice: CodeChoice) -> Bool {
-        code.contains { $0 == choice }
+        guess.contains { $0 == choice }
     }
 }
