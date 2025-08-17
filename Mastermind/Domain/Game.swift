ced2e2155dc60ada1f6092de3d590c94f1ed3c67
Jon Reid <jon@qualitycoding.org>
2025-08-17 10:06:40 -0700

    ^ r Make numberOfRounds parameter explicit in Game initialization

--- Mastermind/Domain/Game.swift
+++ Mastermind/Domain/Game.swift
@@ -20,7 +20,7 @@ final class Game {
         self.secretMaker = secretMaker
         self.secretSize = secretSize
         try codeChoices = CodeChoiceGenerator.generate(from: codeColors, take: numberOfCodeChoices)
-        rounds = Rounds(secretSize: secretSize)
+        rounds = Rounds(secretSize: secretSize, numberOfRounds: 1)
     }
 
     func codeChoice(_ index: Int) -> CodeChoice {
