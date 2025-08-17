301c2add3ce377495251ddac6717eab5db163b6a
Jon Reid <jon@qualitycoding.org>
2025-08-17 10:17:29 -0700

    ^ r Remove default value from Rounds numberOfRounds parameter

--- Mastermind/Domain/Rounds.swift
+++ Mastermind/Domain/Rounds.swift
@@ -1,7 +1,7 @@
 final class Rounds {
     var rounds: [Round]
 
-    init(secretSize: Int, numberOfRounds: Int = 1) {
+    init(secretSize: Int, numberOfRounds: Int) {
         self.rounds = (0..<numberOfRounds).map { _ in Round(secretSize: secretSize) }
     }
 
