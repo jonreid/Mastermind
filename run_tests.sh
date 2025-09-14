7330b6aa0c7a3f675d3163ba52efa70758ab38bb
Jon Reid <jon@qualitycoding.org>
2025-09-14 09:29:31 -0700

    . e run_tests now does pre-check

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,9 @@
 SCHEME='Mastermind'
 DESTINATION='platform=iOS Simulator,OS=18.5,name=iPhone 16'
 
+if grep -R -n --include='*Tests.swift' -E '^\s*// (Arrange|Act|Assert|Given|When|Then)\b' . ; then
+  echo "\nError: Forbidden test comments detected. Remove these comments from tests."
+  exit 1
+fi
+
 set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
