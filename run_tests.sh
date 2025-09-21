b8510037bbe154f8df763d52f1c27700c642a59e
Jon Reid <jon@qualitycoding.org>
2025-09-21 09:06:49 -0700

    . e Improve output of test failure rule

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ SCHEME='Mastermind'
 DESTINATION='platform=iOS Simulator,OS=18.5,name=iPhone 16'
 
 if grep -R -n --include='*Tests.swift' -E '^\s*// (Arrange|Act|Assert|Given|When|Then)\b' . ; then
-  echo "\nError: Forbidden test comments detected. Remove these comments from tests."
+  echo "Error: Forbidden test comments detected. Remove these comments from tests."
   exit 1
 fi
 
