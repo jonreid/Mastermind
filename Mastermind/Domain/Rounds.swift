e257718c2e4d6e85ebf01aeca42804d6f9b5ebe7
Jon Reid <jon@qualitycoding.org>
2025-09-28 09:43:27 -0700

    ^ r Simplify mapping over range

--- Mastermind/Domain/Rounds.swift
+++ Mastermind/Domain/Rounds.swift
@@ -2,8 +2,8 @@ final class Rounds {
     private let rounds: [Round]
 
     init(secretSize: Int, numberOfRounds: Int) {
-        self.rounds = (0..<numberOfRounds).map {
-            round in Round(secretSize: secretSize, roundNumber: round + 1)
+        self.rounds = (1...numberOfRounds).map {
+            round in Round(secretSize: secretSize, roundNumber: round)
         }
     }
 
