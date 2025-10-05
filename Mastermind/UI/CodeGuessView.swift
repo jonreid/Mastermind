014913f632a817b66fc6353a06042ace515d3a17
Jon Reid <jon@qualitycoding.org>
2025-10-05 09:57:14 -0700

    . r Move CodeGuessView into own file

---
+++ Mastermind/UI/CodeGuessView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct CodeGuessView: View {
+    let round: Round
+
+    var body: some View {
+        HStack {
+            Text("\(round.roundNumber.value)").accessibilityIdentifier("roundNumber")
+            ForEach(0 ..< round.size, id: \.self) { index in
+                Button(action: {}, label: {
+                    Circle()
+                        .padding(5)
+                        .overlay(
+                            Circle()
+                                .strokeBorder(Color.unselected, lineWidth: 2)
+                        )
+                        .foregroundColor(round[index]?.color ?? Color.unselected)
+                        .frame(width: 40, height: 40)
+                })
+                .id("guess\(round.roundNumber.value)-\(index + 1)")
+            }
+        }.accessibilityIdentifier("round\(round.roundNumber.value)")
+    }
+}
