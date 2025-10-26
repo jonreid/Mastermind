e9a899de4a7c3f5f3ca2b41ead77dd1c0c77ffcb
Jon Reid <jon@qualitycoding.org>
2025-10-26 10:06:48 -0700

    . e Report total passing test count on success

--- build_and_test.sh
+++ build_and_test.sh
@@ -11,7 +11,10 @@ fi
 
 TMP_OUTPUT=$(mktemp)
 if xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' > "$TMP_OUTPUT" 2>&1; then
-  echo "✅ All tests passed."
+  XCTEST_COUNT=$(grep -A 1 "Test Suite 'All tests' passed" "$TMP_OUTPUT" | grep -Eo 'Executed [0-9]+' | grep -Eo '[0-9]+' || echo "0")
+  SWIFT_TEST_COUNT=$(grep "✔ Test run with" "$TMP_OUTPUT" | grep -Eo 'with [0-9]+ tests' | grep -Eo '[0-9]+' || echo "0")
+  TOTAL_TESTS=$((XCTEST_COUNT + SWIFT_TEST_COUNT))
+  echo "✅ All tests passed: $TOTAL_TESTS tests."
   EXIT_CODE=0
 else
   cat "$TMP_OUTPUT"
