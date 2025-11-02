e216bd5edcf7801b012143dcdc3d2fb1c0cc9a49
Jon Reid <jon@qualitycoding.org>
2025-11-02 09:42:21 -0800

    . e Format awk script for human readability

--- build_and_test.sh
+++ build_and_test.sh
@@ -11,7 +11,26 @@ fi
 
 TMP_OUTPUT=$(mktemp)
 if xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' > "$TMP_OUTPUT" 2>&1; then
-  read -r XCTEST_COUNT SWIFT_TEST_COUNT <<< $(awk '/Executed [0-9]+/ {split($0, a, "Executed "); split(a[2], b, " "); xctest=b[1]} /Test Suite .All tests. passed/ {xctest_final=xctest} /✔ Test run with/ {split($0, a, "with "); split(a[2], b, " "); swift=b[1]} END {print xctest_final+0, swift+0}' "$TMP_OUTPUT")
+  read -r XCTEST_COUNT SWIFT_TEST_COUNT <<< $(
+    awk '
+      /Executed [0-9]+/ {
+        split($0, a, "Executed ")
+        split(a[2], b, " ")
+        xctest=b[1]
+      }
+      /Test Suite .All tests. passed/ {
+        xctest_final=xctest
+      }
+      /✔ Test run with/ {
+        split($0, a, "with ")
+        split(a[2], b, " ")
+        swift=b[1]
+      }
+      END {
+        print xctest_final+0, swift+0
+      }
+    ' "$TMP_OUTPUT"
+  )
   TOTAL_TESTS=$((XCTEST_COUNT + SWIFT_TEST_COUNT))
   echo "✅ All tests passed: $TOTAL_TESTS tests."
   EXIT_CODE=0
